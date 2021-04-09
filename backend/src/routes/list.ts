991a78dbb5f1a1df1079a0b21a1a71ecafc1e9bc
GangbacOl <gangyeonjo336@gmail.com>
2021-04-09 09:14:23 +0900

    feat: Add try catch logic

--- backend/src/routes/list.ts
+++ backend/src/routes/list.ts
@@ -4,8 +4,13 @@ import { getObjectList } from '../middlewares/s3';
 const router = express.Router();
 
 router.get('/', async (req: Request, res: Response) => {
-    const objectList = await getObjectList();
-    res.status(200).json({ posts: objectList });
+    try {
+        const objectList = await getObjectList();
+        res.status(200).json({ posts: objectList });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: '서버 에러' });
+    }
 });
 
 export default router;
