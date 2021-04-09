8667489695c36b251cd01309e432999425e444f8
GangbacOl <gangyeonjo336@gmail.com>
2021-04-09 09:18:00 +0900

    feat: Add upload, delete Router

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import logger from 'morgan';
 
 import listRouter from './routes/list';
+import uploadRouter from './routes/upload';
+import deleteRouter from './routes/delete';
 
 const app = express();
 
@@ -10,6 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/list', listRouter);
+app.use('/upload', uploadRouter);
+app.use('/delete', deleteRouter);
 
 app.get('/', (req: Request, res: Response): void => {
     res.status(200).json({ message: 'success' });
