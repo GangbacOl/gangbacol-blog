432c6df48d2e950fb7c6f57a29f60b81c34d24c2
GangbacOl <gangyeonjo336@gmail.com>
2021-04-23 19:02:11 +0900

    feat: Add Posts table schema

---
+++ backend/src/models/Posts.ts
@@ -0,0 +1,15 @@
+import { Table, Column, Model, PrimaryKey, DataType, AutoIncrement } from 'sequelize-typescript';
+
+@Table
+export default class FilePosts extends Model {
+    @PrimaryKey
+    @AutoIncrement
+    @Column
+    id: number;
+
+    @Column
+    title: string;
+
+    @Column(DataType.TEXT)
+    markdownUrls: string;
+}
