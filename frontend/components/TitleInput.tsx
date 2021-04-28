aa697b5f76b68ccf04d77123613cb72050dd447f
GangbacOl <gangyeonjo336@gmail.com>
2021-04-28 12:08:42 +0900

    feat: Add TitleInput component

---
+++ frontend/components/TitleInput.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+interface Props {
+    title: string;
+    setTitle: Function;
+}
+
+const TitleInput = ({ title, setTitle }: Props) => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => setTitle(e.target.value);
+    return (
+        <Container>
+            <Input type="text" value={title} onChange={handleChange} />
+        </Container>
+    );
+};
+
+const Container = styled.div``;
+const Input = styled.input``;
+
+export default TitleInput;
