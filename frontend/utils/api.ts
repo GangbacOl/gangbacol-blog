93487fe1b36885715d0e596c41c9913bde063d64
GangbacOl <gangyeonjo336@gmail.com>
2021-05-10 15:46:04 +0900

    feat: Add getPosts, getSinglePost axios request function

--- frontend/utils/api.ts
+++ frontend/utils/api.ts
@@ -1,5 +1,13 @@
 import axios from 'axios';
 
-import { BASEURL } from './baseUrl';
+import { GET_SINGLE_POST, GET_POSTS } from './baseUrl';
 
-export const getUser = async (data: number): Promise<object> => await axios.get(`${BASEURL}`, { data });
+export const getSinglePost = async (id: string | string[]): Promise<{}[]> => {
+    console.log(id);
+    const { data } = await axios.get(`${GET_SINGLE_POST}/${id}`);
+    return data;
+};
+export const getPosts = async (): Promise<{}[]> => {
+    const { data } = await axios.get(`${GET_POSTS}`);
+    return data;
+};
