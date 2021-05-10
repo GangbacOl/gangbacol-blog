da14a477b5266ac5d70f03191ea7c3f5c985edfa
GangbacOl <gangyeonjo336@gmail.com>
2021-05-10 09:54:45 +0900

    feat: Add get one item controller.

--- backend/src/routes/s3/s3.controller.ts
+++ backend/src/routes/s3/s3.controller.ts
@@ -1,18 +1,26 @@
 import { Request, Response } from 'express';
 
-import { deleteObject, getObjectList } from '../../middlewares/s3';
+import { deleteObject } from '../../middlewares/s3';
 import MulterRequest from '../../../interface/MulterRequest.interface';
 
 import { postsRepository } from '../../models/index';
 
+const getOneItemCtrl = async (req: Request, res: Response) => {
+    try {
+        const id = req.body.id;
+        const result = await postsRepository.findOne({ where: { id } });
+        res.status(200).json({ result });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: '서버 에러' });
+    }
+};
+
 const getItemCtrl = async (req: Request, res: Response) => {
     try {
-        const objectList = await getObjectList();
         const result = await postsRepository.findAll({ raw: true });
-        (result as any[]).map((item, idx) => {
-            objectList[idx].title = item.title;
-        });
-        res.status(200).json({ posts: objectList });
+        console.log(result);
+        res.status(200).json({ posts: result });
     } catch (err) {
         console.log(err);
         res.status(500).json({ message: '서버 에러' });
@@ -21,19 +29,22 @@ const getItemCtrl = async (req: Request, res: Response) => {
 
 const uploadImagesCtrl = async (req: MulterRequest, res: Response) => {
     try {
+        console.log(req.files);
         const filesLocation = (req.files as any[]).map((file) => file.location);
-        res.json({ filesLocation });
+        res.status(200).json({ filesLocation });
     } catch (err) {
         console.log(err);
         res.status(500).json({ message: '서버 에러' });
     }
 };
 
-const uploadMarkdownCtrl = async (req: MulterRequest, res: Response) => {
+const uploadMarkdownCtrl = async (req: Request, res: Response) => {
     try {
-        const title = req.body.title;
-        const result = await postsRepository.create({ title, markdownUrls: req.file.location });
-        res.status(200).json({ message: req.file.location });
+        const { title, content } = req.body;
+        console.log(title);
+        console.log(content);
+        const result = await postsRepository.create({ title, content });
+        res.status(200).json({ message: 'success' });
     } catch (err) {
         console.log(err);
         res.status(500).json({ message: '서버 에러' });
@@ -51,4 +62,4 @@ const deleteItemCtrl = async (req: Request, res: Response) => {
     }
 };
 
-export default { getItemCtrl, uploadImagesCtrl, uploadMarkdownCtrl, deleteItemCtrl };
+export default { getOneItemCtrl, getItemCtrl, uploadImagesCtrl, uploadMarkdownCtrl, deleteItemCtrl };
