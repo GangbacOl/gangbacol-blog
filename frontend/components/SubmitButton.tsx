494afe9b0eecc2915af593c751e7e3b5ee428b27
GangbacOl <gangyeonjo336@gmail.com>
2021-05-10 15:38:25 +0900

    feat: Add images, markdown uploader

--- frontend/components/SubmitButton.tsx
+++ frontend/components/SubmitButton.tsx
@@ -1,28 +1,43 @@
-import { useState } from 'react';
+import { useState, memo } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
+
+import { UPLOAD_IMAGE_URL, UPLOAD_MARKDOWN_URL } from '../utils/baseUrl';
 
 interface Props {
     title: string;
-    images: File | null;
+    images: FileList | null;
     markdown: FileList | null;
 }
 
 const SubmitButton = ({ title, images, markdown }: Props) => {
-    const [text, setText] = useState<string>('');
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
     const handleClick = () => {
-        let fileReader = new FileReader();
-        if (markdown) {
-            console.log(markdown[0]);
-            fileReader.readAsText(markdown[0]);
-            fileReader.onload = function (e: any) {
-                setText(e.target.result);
-            };
-        }
+        if (!markdown || !images) return;
+        setIsLoading(true);
+
+        const fileReader = new FileReader();
+        fileReader.readAsText(markdown[0]);
+        fileReader.onload = async (e: any) => {
+            let content = e.target.result;
+
+            const formDataImage = new FormData();
+            Array.from(images).forEach((image) => formDataImage.append('images', image));
+            const res1 = await axios.post(UPLOAD_IMAGE_URL, formDataImage);
+
+            res1.data.filesLocation.map((item: string) => {
+                const replaceText = `<img src="${item}">`;
+                content = content.replace(/!\[[^\]]*\]\((.*?)\s*("(?:.*[^"])")?\s*\)/, replaceText);
+            });
+
+            const data = { title, content };
+            await axios.post(UPLOAD_MARKDOWN_URL, data);
+        };
     };
     return (
         <Container>
             <Button type="button" value="저장" onClick={handleClick} />
-            <p>{text.replace(/!\[[^\]]*\]\((.*?)\s*("(?:.*[^"])")?\s*\)/, '이미지 링크')}</p>
         </Container>
     );
 };
@@ -30,4 +45,4 @@ const SubmitButton = ({ title, images, markdown }: Props) => {
 const Container = styled.div``;
 const Button = styled.input``;
 
-export default SubmitButton;
+export default memo(SubmitButton);
