1b2dc96eff64dc920044cab1b7a175bb466c25de
GangbacOl <gangyeonjo336@gmail.com>
2021-05-10 15:07:21 +0900

    chore: Add api urls

--- frontend/utils/baseUrl.ts
+++ frontend/utils/baseUrl.ts
@@ -1,1 +1,7 @@
-export const BASEURL: string = '';
+export const BASEURL: string = 'http://localhost:5000';
+
+export const GET_SINGLE_POST: string = `${BASEURL}/item`;
+export const GET_POSTS: string = `${BASEURL}/list`;
+export const UPLOAD_IMAGE_URL: string = `${BASEURL}/uploadImages`;
+export const UPLOAD_MARKDOWN_URL: string = `${BASEURL}/uploadMarkdown`;
+export const DELETE_URL: string = `${BASEURL}/delete`;
