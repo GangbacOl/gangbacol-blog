99ae12dcf5024ccaada383aae98fd39cc099c6bb
GangbacOl <gangyeonjo336@gmail.com>
2021-05-26 00:19:31 +0900

    feat: Add validator

---
+++ backend/src/routes/auth/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import { check } from 'express-validator';
+
+import controller from './auth.controller';
+
+const router = express.Router();
+
+const registerValidateList = [
+    check('username', 'MissingParameter').notEmpty(),
+    check('account', 'MissingParameter').notEmpty(),
+    check('password', 'MissingParameter').notEmpty(),
+];
+
+const loginValidateList = [
+    check('account', 'MissingParameter').notEmpty(),
+    check('password', 'MissingParameter').notEmpty(),
+];
+
+router.post('/register', registerValidateList, controller.registerCtrl);
+router.post('/login', loginValidateList, controller.loginCtrl);
+
+export default router;
---
+++ backend/src/validator/auth.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import _passwordValidator from 'password-validator';
+import { validationResult } from 'express-validator';
+
+export const passwordValidator = (password: string) => {
+    const schema = new _passwordValidator();
+    schema.is().min(8).is().max(50).has().uppercase().has().lowercase().has().digits().has().not().spaces();
+
+    return !schema.validate(password);
+};
+
+export const bodyValidator = (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+};
