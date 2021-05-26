09f887b9149e8a2fd7635475514c74d906fd72e2
GangbacOl <gangyeonjo336@gmail.com>
2021-05-26 11:46:27 +0900

    feat: Add token verification middleware

---
+++ backend/src/middlewares/auth.ts
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const verifyToken = async (req, res, next) => {
+    const token = req.headers['access-token'];
+
+    if (!token) {
+        return res.status(403).json({
+            success: false,
+            message: 'not logged in',
+        });
+    }
+
+    await jwt.verify(token, process.env.SECRET_KEY, (error, decoded) => {
+        if (error)
+            return res.status(403).json({
+                success: false,
+                message: error.message,
+            });
+        req.decoded = decoded;
+        next();
+    });
+};
--- backend/src/routes/s3/index.ts
+++ backend/src/routes/s3/index.ts
@@ -2,8 +2,14 @@ import express from 'express';
 
 import { uploadImages } from '../../middlewares/s3';
 import controller from './s3.controller';
+import { verifyToken } from '../../middlewares/auth';
 
 const router = express.Router();
+
+router.use('/list', verifyToken);
+router.use('/uploadImages', verifyToken);
+router.use('/uploadMarkdown', verifyToken);
+router.use('/delete/:id', verifyToken);
 
 router.get('/item/:id', controller.getOneItemCtrl);
 router.get('/list', controller.getItemCtrl);
