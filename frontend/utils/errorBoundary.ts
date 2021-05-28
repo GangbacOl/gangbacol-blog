744d639992b119fbf467e0e14aae6b5a2a7a8c33
GangbacOl <gangyeonjo336@gmail.com>
2021-05-28 15:45:12 +0900

    chore: Add convertErrorToMsg function

---
+++ frontend/utils/errorBoundary.ts
@@ -0,0 +1,14 @@
+export const convertErrorToMsg = (errorCode: string) => {
+    switch (errorCode) {
+        case 'MissingParameter':
+            return '모든 입력칸을 채워주세요.';
+        case 'ExistingAccount':
+            return '이미 존재하는 계정입니다.';
+        case 'InvalidPassword':
+            return '패스워드 형식이 맞지 않습니다.\n 8자~50자, 대문자 포함';
+        case 'UserNotFound':
+            return '존재하지 않는 계정입니다.';
+        case 'PasswordNotCorrect':
+            return '패스워드가 일치하지 않습니다.';
+    }
+};
