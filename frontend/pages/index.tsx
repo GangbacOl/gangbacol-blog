dce9a1070b309dbb985cb1bbcc22e6c490996fd2
GangbacOl <gangyeonjo336@gmail.com>
2021-05-28 15:55:25 +0900

    feat: Update jsx, isAdmin logic

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,35 +1,57 @@
+import { useState, useLayoutEffect } from 'react';
 import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 import { GetServerSideProps } from 'next';
 import { END } from 'redux-saga';
 
-import wrapper from '../store/configStore';
 import PostList from '../components/PostList';
+import { SagaStore, wrapper } from '../store/configStore';
 import { AsyncActionEnum } from '../interfaces/reducer/action.interface';
 import { PostStoreType } from '../interfaces/reducer/state.interface';
+
+import Empty from '../assets/empty.svg';
 
 interface StoreType {
     post: PostStoreType;
 }
 
 const IndexPage = () => {
+    const [isAdmin, setIsAdmin] = useState<boolean>(false);
+
     const posts = useSelector((state: StoreType) => state.post.posts);
-    console.log(posts);
+
+    useLayoutEffect(() => {
+        const isToken = localStorage.getItem('token') ? true : false;
+        setIsAdmin(isToken);
+    }, []);
     return (
         <Container>
-            <PostList posts={posts} />
+            {posts.length > 0 ? (
+                <PostList posts={posts} isAdmin={isAdmin} />
+            ) : (
+                <EmptyContainer>
+                    <EmptySvgWrap>
+                        <EmptySvg />
+                    </EmptySvgWrap>
+                    <Description>작성된 게시물이 없습니다.</Description>
+                </EmptyContainer>
+            )}
         </Container>
     );
 };
 
 const Container = styled.div``;
+const EmptyContainer = styled.div``;
+const EmptySvgWrap = styled.div``;
+const EmptySvg = styled(Empty)``;
+const Description = styled.p``;
 
-export const getServerSideProps: GetServerSideProps = wrapper.getServerSideProps(async ({ store }) => {
-    store.dispatch({
+export const getServerSideProps: GetServerSideProps = wrapper.getServerSideProps(async (ctx) => {
+    ctx.store.dispatch({
         type: AsyncActionEnum.LOAD_POSTS_REQUEST,
     });
-    store.dispatch(END);
-    await (store as any).sagaTask.toPromise();
+    ctx.store.dispatch(END);
+    await (ctx.store as SagaStore).sagaTask.toPromise();
 });
 
 export default IndexPage;
