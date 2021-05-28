8e9c9b050595f17a16c732e9894759a6450d9ece
GangbacOl <gangyeonjo336@gmail.com>
2021-05-28 15:58:36 +0900

    chore: Update jsx

--- frontend/components/TitleInput.tsx
+++ frontend/components/TitleInput.tsx
@@ -10,12 +10,14 @@ const TitleInput = ({ title, setTitle }: Props) => {
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => setTitle(e.target.value);
     return (
         <Container>
+            <Title>제목</Title>
             <Input type="text" value={title} onChange={handleChange} />
         </Container>
     );
 };
 
 const Container = styled.div``;
+const Title = styled.h2``;
 const Input = styled.input``;
 
 export default memo(TitleInput);
