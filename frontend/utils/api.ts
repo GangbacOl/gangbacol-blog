a0320f841680076b8bbb8e4ea6c05936943ed4af
GangbacOl <gangyeonjo336@gmail.com>
2021-05-28 15:46:49 +0900

    feat: Add uploadPostImages, uploadPost, deletePost, login, register api

--- frontend/utils/api.ts
+++ frontend/utils/api.ts
@@ -1,13 +1,60 @@
 import axios from 'axios';
 
-import { GET_SINGLE_POST, GET_POSTS } from './baseUrl';
+import {
+    UPLOAD_IMAGE_URL,
+    UPLOAD_MARKDOWN_URL,
+    GET_SINGLE_POST,
+    GET_POSTS,
+    LOGIN_URL,
+    REGISTER_URL,
+    DELETE_URL,
+} from './baseUrl';
+
+export const uploadPostImages = async (images: FileList) => {
+    const formDataImage = new FormData();
+    Array.from(images).forEach((image) => formDataImage.append('images', image));
+
+    const { data } = await axios.post(UPLOAD_IMAGE_URL, formDataImage, {
+        headers: { authorization: localStorage.getItem('token') },
+    });
+
+    return data;
+};
+
+export const uploadPost = async (title: string, content: string, filenames: string[]) => {
+    return await axios.post(
+        UPLOAD_MARKDOWN_URL,
+        { title, content, filenames },
+        { headers: { authorization: localStorage.getItem('token') } }
+    );
+};
 
 export const getSinglePost = async (id: string | string[]): Promise<{}[]> => {
-    console.log(id);
     const { data } = await axios.get(`${GET_SINGLE_POST}/${id}`);
     return data;
 };
+
 export const getPosts = async (): Promise<{}[]> => {
     const { data } = await axios.get(`${GET_POSTS}`);
+    console.log(data);
     return data;
+};
+
+export const deletePost = async (id: number) => {
+    const res = await axios.delete(`${DELETE_URL}/${id}`, {
+        headers: { authorization: localStorage.getItem('token') },
+    });
+    return res;
+};
+
+export const login = async (data: { account: string; password: string }) => {
+    const response = await axios.post(`${LOGIN_URL}`, data, {
+        withCredentials: true,
+    });
+    return response;
+};
+
+export const register = async (data: { username: string; account: string; password: string }) => {
+    const response = await axios.post(`${REGISTER_URL}`, data);
+    return response;
 };
