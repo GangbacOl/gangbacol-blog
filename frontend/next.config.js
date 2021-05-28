60c4244cadc23335debe5c705a62337b3b2e8b11
GangbacOl <gangyeonjo336@gmail.com>
2021-05-28 15:33:49 +0900

    chore: Update next config for using svg

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,21 @@
 module.exports = {
     webpack(config) {
-        config.module.rules.push({
-            test: /\.(png|jpeg|gif|jp2|webp)$/,
-            loader: 'file-loader',
-            options: {
-                name: '[name].[ext]',
+        config.module.rules.push(
+            {
+                test: /\.svg$/,
+                issuer: {
+                    test: /\.(js|ts|jsx|tsx)x?$/,
+                },
+                use: ['@svgr/webpack'],
             },
-        });
+            {
+                test: /\.(png|jpeg|gif|jp2|webp)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                },
+            }
+        );
         return config;
     },
 };
