f8b05cb998af9eb89917526b627313ca21f2edb2
GangbacOl <gangyeonjo336@gmail.com>
2021-06-01 00:44:40 +0900

    feat: Update global style(font size, font family)

--- frontend/style/globalStyle.ts
+++ frontend/style/globalStyle.ts
@@ -1,8 +1,19 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
+    @import url(//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css);
+    html, * { font-family: 'Spoqa Han Sans Neo', 'sans-serif'; }
     body {
         background: #f0eff5;
         margin: 0;
+    }
+    @media only screen and (min-width: 1024px) {
+        html { font-size: 16px; }
+    }
+    @media only screen and (max-width: 1024px) {
+        html { font-size: 14px; }
+    }
+    @media only screen and (max-width: 450px) {
+        html { font-size: 12px; }
     }
 `;
