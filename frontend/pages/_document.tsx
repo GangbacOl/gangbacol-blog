6258ff871d7a46d1d9b9259d95927f8d3afb1965
GangbacOl <gangyeonjo336@gmail.com>
2021-06-01 01:29:32 +0900

    feat: Add Head (title, viewport, open graph, favicon)

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -4,7 +4,17 @@ class MyDocument extends Document {
     render() {
         return (
             <Html>
-                <Head />
+                <Head>
+                    <title>GangbacOl Blog</title>
+                    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                    <meta property="og:title" content="GangbacOl Blog" />
+                    <meta
+                        property="og:description"
+                        content="배움과 경험을 글로 정리하는 GangbacOl의 블로그입니다."
+                    />
+                    <meta property="og:image" content="/assets/og-background.jpg" />
+                    <link rel="shortcut icon" href="/favicons/favicon.png" />
+                </Head>
                 <body>
                     <Main />
                     <NextScript />
