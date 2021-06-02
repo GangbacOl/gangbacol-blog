138832f165867eadd6b10e5184815aafd35e70e4
GangbacOl <gangyeonjo336@gmail.com>
2021-06-02 11:56:17 +0900

    fix: Add getInitialProps function for fixing flash issue of styled-component

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,27 +1,28 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-class MyDocument extends Document {
-    render() {
-        return (
-            <Html>
-                <Head>
-                    <title>GangbacOl Blog</title>
-                    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-                    <meta property="og:title" content="GangbacOl Blog" />
-                    <meta
-                        property="og:description"
-                        content="배움과 경험을 글로 정리하는 GangbacOl의 블로그입니다."
-                    />
-                    <meta property="og:image" content="/assets/og-background.jpg" />
-                    <link rel="shortcut icon" href="/favicons/favicon.png" />
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
+export default class MyDocument extends Document {
+    static async getInitialProps(ctx: any) {
+        const sheet = new ServerStyleSheet();
+        const originalRenderPage = ctx.renderPage;
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: (App: any) => (props: any) => sheet.collectStyles(<App {...props} />), //ServerStyleSheet를 사용해 정의된 모든 스타일을 수집하여 페이지가 렌더링되기 전에 props로 채워지도록 반환한다.
+                });
+            const initialProps = await Document.getInitialProps(ctx);
+
+            return {
+                ...initialProps,
+                styles: (
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                    </>
+                ),
+            };
+        } finally {
+            sheet.seal();
+        }
     }
 }
-
-export default MyDocument;
