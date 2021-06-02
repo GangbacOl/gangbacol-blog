87fc7a7424fe4fbf4e15447fb1409a3a7546b50e
GangbacOl <gangyeonjo336@gmail.com>
2021-06-02 11:56:37 +0900

    feat: Add Head (title, viewport, open graph, favicon)

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import GlobalStyle from '../style/globalStyle';
 import { wrapper } from '../store/configStore';
@@ -6,6 +7,17 @@ import { wrapper } from '../store/configStore';
 const App = ({ Component, pageProps }: AppProps) => {
     return (
         <>
+            <Head>
+                <title>GangbacOl Blog</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <meta property="og:title" content="GangbacOl Blog" />
+                <meta
+                    property="og:description"
+                    content="배움과 경험을 글로 정리하는 GangbacOl의 블로그입니다."
+                />
+                <meta property="og:image" content="/assets/og-background.jpg" />
+                <link rel="shortcut icon" href="/favicons/favicon.png" />
+            </Head>
             <GlobalStyle />
             <Component {...pageProps} />
         </>
