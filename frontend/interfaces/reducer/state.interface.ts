0602bc8b4bc591612953f54d1fb58d2c63951f42
GangbacOl <gangyeonjo336@gmail.com>
2021-10-10 02:46:26 +0900

    chore: Update postStoreInterface interface and Add AsyncRequestStatus type

--- frontend/interfaces/reducer/state.interface.ts
+++ frontend/interfaces/reducer/state.interface.ts
@@ -1,15 +1,22 @@
-export type Post = {
-    id: number;
-    title: string;
-    description: string;
-    content: string;
-    createdAt: string;
-    updatedAt: string;
+export type AsyncRequestStatus = {
+  loading: boolean;
+  data: object | null;
+  error: object | null;
 };
 
-export interface PostStoreType {
-    singlePost: Post | null;
-    posts: Post[];
-    loading: boolean;
-    error: {} | null;
+export type Post = {
+  id: number;
+  title: string;
+  description: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export interface PostStoreInterface {
+  singlePost: Post | null;
+  multiplePost: Post[];
+  getSinglePostStatus: AsyncRequestStatus;
+  getMultiplePostStatus: AsyncRequestStatus;
+  deletePostStatus: AsyncRequestStatus;
 }
