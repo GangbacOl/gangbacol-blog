4fb00a33b4b6de35e66f19c7264a5f8955438a4d
GangbacOl <gangyeonjo336@gmail.com>
2021-10-10 02:49:10 +0900

    feat: Update post initialstate, postReducer

--- frontend/store/modules/post.ts
+++ frontend/store/modules/post.ts
@@ -1,39 +1,59 @@
-import { createReducer } from 'typesafe-actions';
-import { HYDRATE } from 'next-redux-wrapper';
-import { AsyncActionEnum, AsyncActionType } from '../../interfaces/reducer/action.interface';
-import { PostStoreType } from '../../interfaces/reducer/state.interface';
+import { createReducer } from "typesafe-actions";
+import { HYDRATE } from "next-redux-wrapper";
 
-export const initialState: PostStoreType = {
-    singlePost: null,
-    posts: [],
-    loading: false,
-    error: null,
+import asyncReqStatus from "../../utils/asyncReqStatus";
+import { AsyncActionEnum, AsyncActionType } from "../../interfaces/reducer/action.interface";
+import { PostStoreInterface } from "../../interfaces/reducer/state.interface";
+
+export const initialState: PostStoreInterface = {
+  singlePost: null,
+  multiplePost: [],
+  getSinglePostStatus: asyncReqStatus.initial(),
+  getMultiplePostStatus: asyncReqStatus.initial(),
+  deletePostStatus: asyncReqStatus.initial(),
 };
 
-const postReducer = createReducer<PostStoreType, AsyncActionType>(initialState, {
-    [HYDRATE]: (state, action) => ({ ...state, ...action.payload.post }),
-    [AsyncActionEnum.LOAD_POSTS_REQUEST]: (state) => ({ ...state, loading: true }),
-    [AsyncActionEnum.LOAD_POSTS_SUCCESS]: (state, action) => ({
-        ...state,
-        posts: action.payload,
-        loading: false,
-    }),
-    [AsyncActionEnum.LOAD_POSTS_FAILURE]: (state, action) => ({
-        ...state,
-        error: action.payload,
-        loading: false,
-    }),
-    [AsyncActionEnum.LOAD_SINGLE_POST_REQUEST]: (state) => ({ ...state, loading: true }),
-    [AsyncActionEnum.LOAD_SINGLE_POST_SUCCESS]: (state, action) => ({
-        ...state,
-        singlePost: action.payload,
-        loading: false,
-    }),
-    [AsyncActionEnum.LOAD_SINGLE_POST_FAILURE]: (state, action) => ({
-        ...state,
-        error: action.payload,
-        loading: false,
-    }),
+const postReducer = createReducer<PostStoreInterface, AsyncActionType>(initialState, {
+  [HYDRATE]: (state, action) => ({ ...state, ...action.payload.post }),
+  // get single post
+  [AsyncActionEnum.GET_SINGLE_POST_REQUEST]: (state) => ({
+    ...state,
+    getSinglePostStatus: asyncReqStatus.loading(),
+  }),
+  [AsyncActionEnum.GET_SINGLE_POST_SUCCESS]: (state, action) => ({
+    ...state,
+    singlePost: action.payload,
+    getSinglePostStatus: asyncReqStatus.success(action.payload),
+  }),
+  [AsyncActionEnum.GET_SINGLE_POST_FAILURE]: (state, action) => ({
+    ...state,
+    getSinglePostStatus: asyncReqStatus.failure(action.payload),
+  }),
+  // get multiple post
+  [AsyncActionEnum.GET_MULTIPLE_POST_REQUEST]: (state) => ({
+    ...state,
+    getMultiplePostStatus: asyncReqStatus.loading(),
+  }),
+  [AsyncActionEnum.GET_MULTIPLE_POST_SUCCESS]: (state, action) => ({
+    ...state,
+    multiplePost: action.payload,
+    getMultiplePostStatus: asyncReqStatus.success(action.payload),
+  }),
+  [AsyncActionEnum.GET_MULTIPLE_POST_FAILURE]: (state, action) => ({
+    ...state,
+    getMultiplePostStatus: asyncReqStatus.failure(action.payload),
+  }),
+  // delete post
+  [AsyncActionEnum.DELETE_POST_REQUEST]: (state) => ({
+    ...state,
+    getMultiplePostStatus: asyncReqStatus.loading(),
+  }),
+  [AsyncActionEnum.DELETE_POST_SUCCESS]: (state, action) => ({
+    ...state,
+  }),
+  [AsyncActionEnum.DELETE_POST_FAILURE]: (state, action) => ({
+    ...state,
+  }),
 });
 
 export default postReducer;
