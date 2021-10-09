4094ba683480008e6559359fc371e50ef36a0835
GangbacOl <gangyeonjo336@gmail.com>
2021-10-09 23:13:39 +0900

    feat: Add deletePost api

--- frontend/utils/api.ts
+++ frontend/utils/api.ts
@@ -1,62 +1,62 @@
-import axios from 'axios';
+import axios from "axios";
 
 import {
-    UPLOAD_IMAGE_URL,
-    UPLOAD_MARKDOWN_URL,
-    GET_SINGLE_POST,
-    GET_POSTS,
-    LOGIN_URL,
-    REGISTER_URL,
-    DELETE_URL,
-} from './baseUrl';
+  UPLOAD_IMAGE_URL,
+  UPLOAD_MARKDOWN_URL,
+  GET_SINGLE_POST,
+  GET_POSTS,
+  LOGIN_URL,
+  REGISTER_URL,
+  DELETE_URL,
+} from "../constants/baseUrl";
 
 export const uploadPostImages = async (images: FileList) => {
-    const formDataImage = new FormData();
-    Array.from(images).forEach((image) => formDataImage.append('images', image));
+  const formDataImage = new FormData();
+  Array.from(images).forEach((image) => formDataImage.append("images", image));
 
-    const { data } = await axios.post(UPLOAD_IMAGE_URL, formDataImage, {
-        headers: { authorization: localStorage.getItem('token') },
-    });
+  const { data } = await axios.post(UPLOAD_IMAGE_URL, formDataImage, {
+    headers: { authorization: localStorage.getItem("token") },
+  });
 
-    return data;
+  return data;
 };
 
 export const uploadPost = async (
-    title: string,
-    description: string,
-    content: string,
-    filenames: string[]
+  title: string,
+  description: string,
+  content: string,
+  filenames: string[]
 ) => {
-    return await axios.post(
-        UPLOAD_MARKDOWN_URL,
-        { title, description, content, filenames },
-        { headers: { authorization: localStorage.getItem('token') } }
-    );
+  return await axios.post(
+    UPLOAD_MARKDOWN_URL,
+    { title, description, content, filenames },
+    { headers: { authorization: localStorage.getItem("token") } }
+  );
 };
 
 export const getSinglePost = async (id: string | string[]): Promise<{}[]> => {
-    const { data } = await axios.get(`${GET_SINGLE_POST}/${id}`);
-    return data;
+  const { data } = await axios.get(`${GET_SINGLE_POST}/${id}`);
+  return data;
 };
 
 export const getPosts = async (): Promise<{}[]> => {
-    const { data } = await axios.get(`${GET_POSTS}`);
-    return data;
+  const { data } = await axios.get(`${GET_POSTS}`);
+  return data;
 };
 
-export const deletePost = async (id: number) => {
-    const res = await axios.delete(`${DELETE_URL}/${id}`, {
-        headers: { authorization: localStorage.getItem('token') },
-    });
-    return res;
+export const deletePost = async (id: number): Promise<{}> => {
+  const { data } = await axios.delete(`${DELETE_URL}/${id}`, {
+    headers: { authorization: localStorage.getItem("token") },
+  });
+  return data;
 };
 
 export const login = async (data: { account: string; password: string }) => {
-    const response = await axios.post(`${LOGIN_URL}`, data);
-    return response;
+  const response = await axios.post(`${LOGIN_URL}`, data);
+  return response;
 };
 
 export const register = async (data: { username: string; account: string; password: string }) => {
-    const response = await axios.post(`${REGISTER_URL}`, data);
-    return response;
+  const response = await axios.post(`${REGISTER_URL}`, data);
+  return response;
 };
