d1bbaf32350d25e73c6b7cdd8e3dc50cf6339df6
GangbacOl <gangyeonjo336@gmail.com>
2021-10-12 01:27:27 +0900

    style: Update code style

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,28 +1,28 @@
-import Document from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
+import Document from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
-    static async getInitialProps(ctx: any) {
-        const sheet = new ServerStyleSheet();
-        const originalRenderPage = ctx.renderPage;
-        try {
-            ctx.renderPage = () =>
-                originalRenderPage({
-                    enhanceApp: (App: any) => (props: any) => sheet.collectStyles(<App {...props} />), //ServerStyleSheet를 사용해 정의된 모든 스타일을 수집하여 페이지가 렌더링되기 전에 props로 채워지도록 반환한다.
-                });
-            const initialProps = await Document.getInitialProps(ctx);
+  static async getInitialProps(ctx: any) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App: any) => (props: any) => sheet.collectStyles(<App {...props} />), //ServerStyleSheet를 사용해 정의된 모든 스타일을 수집하여 페이지가 렌더링되기 전에 props로 채워지도록 반환한다.
+        });
+      const initialProps = await Document.getInitialProps(ctx);
 
-            return {
-                ...initialProps,
-                styles: (
-                    <>
-                        {initialProps.styles}
-                        {sheet.getStyleElement()}
-                    </>
-                ),
-            };
-        } finally {
-            sheet.seal();
-        }
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
     }
+  }
 }
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,83 +1,80 @@
-import { useState, useLayoutEffect } from 'react';
-import { useSelector } from 'react-redux';
-import styled from 'styled-components';
-import { GetServerSideProps } from 'next';
-import { END } from 'redux-saga';
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import styled from "styled-components";
+import { GetServerSideProps } from "next";
+import { END } from "redux-saga";
 
-import PostList from '../components/PostList';
-import { SagaStore, wrapper } from '../store/configStore';
-import { AsyncActionEnum } from '../interfaces/reducer/action.interface';
-import { PostStoreType } from '../interfaces/reducer/state.interface';
+import PostList from "../components/PostList";
+import { SagaStore, wrapper } from "../store/configStore";
+import { AsyncActionEnum } from "../interfaces/reducer/action.interface";
+import { PostStoreInterface } from "../interfaces/reducer/state.interface";
 
-import Empty from '../public/assets/empty.svg';
+import Empty from "../public/assets/empty.svg";
 
 interface StoreType {
-    post: PostStoreType;
+  post: PostStoreInterface;
 }
 
 const IndexPage = () => {
-    const [isAdmin, setIsAdmin] = useState<boolean>(false);
+  const [isAdmin, setIsAdmin] = useState<boolean>(false);
 
-    const posts = useSelector((state: StoreType) => state.post.posts);
+  const posts = useSelector((state: StoreType) => state.post.multiplePost);
 
-    useLayoutEffect(() => {
-        const isToken = localStorage.getItem('token') ? true : false;
-        setIsAdmin(isToken);
-    }, []);
-    return (
-        <Container>
-            {posts.length > 0 ? (
-                <PostList posts={posts} isAdmin={isAdmin} />
-            ) : (
-                <EmptyContainer>
-                    <EmptySvgWrap>
-                        <EmptySvg />
-                    </EmptySvgWrap>
-                    <Description>작성된 게시물이 없습니다.</Description>
-                </EmptyContainer>
-            )}
-        </Container>
-    );
+  useEffect(() => setIsAdmin(localStorage.getItem("token") ? true : false), []);
+  return (
+    <Container>
+      {posts.length > 0 ? (
+        <PostList posts={posts} isAdmin={isAdmin} />
+      ) : (
+        <EmptyContainer>
+          <EmptySvgWrap>
+            <EmptySvg />
+          </EmptySvgWrap>
+          <Description>작성된 게시물이 없습니다.</Description>
+        </EmptyContainer>
+      )}
+    </Container>
+  );
 };
 
 const Container = styled.div`
-    padding: 40px 20%;
-    @media only screen and (max-width: 1024px) {
-        padding: 20px;
-    }
-    @media only screen and (max-width: 450px) {
-        padding: 10px 20px;
-    }
+  padding: 40px 20%;
+  @media only screen and (max-width: 1024px) {
+    padding: 20px;
+  }
+  @media only screen and (max-width: 450px) {
+    padding: 10px 20px;
+  }
 `;
 const EmptyContainer = styled.div`
-    height: calc(100vh - 80px);
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+  height: calc(100vh - 80px);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 const EmptySvgWrap = styled.div`
-    width: 100px;
-    height: auto;
-    @media only screen and (max-width: 1024px) {
-        width: 90px;
-    }
-    @media only screen and (max-width: 450px) {
-        width: 70px;
-    }
+  width: 100px;
+  height: auto;
+  @media only screen and (max-width: 1024px) {
+    width: 90px;
+  }
+  @media only screen and (max-width: 450px) {
+    width: 70px;
+  }
 `;
 const EmptySvg = styled(Empty)``;
 const Description = styled.p`
-    font-size: 1.3rem;
-    letter-spacing: 1.3x;
+  font-size: 1.3rem;
+  letter-spacing: 1.3x;
 `;
 
 export const getServerSideProps: GetServerSideProps = wrapper.getServerSideProps(async (ctx) => {
-    ctx.store.dispatch({
-        type: AsyncActionEnum.LOAD_POSTS_REQUEST,
-    });
-    ctx.store.dispatch(END);
-    await (ctx.store as SagaStore).sagaTask.toPromise();
+  ctx.store.dispatch({
+    type: AsyncActionEnum.GET_MULTIPLE_POST_REQUEST,
+  });
+  ctx.store.dispatch(END);
+  await (ctx.store as SagaStore).sagaTask.toPromise();
 });
 
 export default IndexPage;
