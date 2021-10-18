1b80b918f7b103f94afcf7ae725dff4804af1633
GangbacOl <gangyeonjo336@gmail.com>
2021-10-18 15:03:15 +0900

    feat: Update page design

--- frontend/pages/post/[id].tsx
+++ frontend/pages/post/[id].tsx
@@ -10,21 +10,43 @@ import { Post as PostType, RootState } from "../../interfaces/reducer/state.inte
 
 const Post = () => {
   const post: PostType | null = useSelector((state: RootState) => state.post.singlePost);
-  return <Container>{post ? <Markdown>{post.content}</Markdown> : null}</Container>;
+  return (
+    <Container>
+      <MarkdownWrapper>{post ? <Markdown>{post.content}</Markdown> : null}</MarkdownWrapper>
+    </Container>
+  );
 };
 
 const Container = styled.div`
+  width: 100%;
+  margin: 40px auto;
+  display: flex;
+  justify-content: center;
+`;
+
+const MarkdownWrapper = styled.div`
   width: 50%;
-  padding: 40px 25%;
+  padding: 30px 30px;
+  background: #f8f8f8;
+  border-radius: 40px;
+  box-shadow: 41px 41px 82px #bebebe, -41px -41px 82px #ffffff;
   overflow: hidden;
   & img {
     margin: 0 auto;
     width: 90%;
     padding: 0 5%;
   }
+  & pre {
+    width: calc(100% - 40px);
+    padding: 20px;
+    border-radius: 10px;
+    background: #eeeeee;
+    color: #3a3a3a;
+    font-weight: 500;
+  }
   @media only screen and (max-width: 1024px) {
-    width: 80%;
-    padding: 40px 10%;
+    width: calc(80% - 60px);
+    padding: 40px 30px;
   }
   @media only screen and (max-width: 450px) {
     width: calc(100% - 60px);
