7fce54a06c7fa460d3ebb03ac8db0d04a4410821
GangbacOl <gangyeonjo336@gmail.com>
2022-03-09 02:32:24 +0900

    chore: verifyToken 함수 제거

--- frontend/apis/auth.ts
+++ frontend/apis/auth.ts
@@ -1,17 +1,8 @@
 import axios from "../utils/axios";
-import { LOGIN_URL, REGISTER_URL, TOKEN_VERIFY_URL } from "../constants/baseUrl";
+import { LOGIN_URL, REGISTER_URL } from "../constants/baseUrl";
 
-export const login = async (data: { account: string; password: string }) => {
-  const response = await axios.post(`${LOGIN_URL}`, data);
-  return response;
-};
+export const login = async (data: { account: string; password: string }) =>
+    await axios.post(`${LOGIN_URL}`, data);
 
-export const register = async (data: { username: string; account: string; password: string }) => {
-  const response = await axios.post(`${REGISTER_URL}`, data);
-  return response;
-};
-
-export const verifyToken = async (token: string) => {
-  const response = await axios.post(`${TOKEN_VERIFY_URL}`, { data: { token } });
-  return response;
-};
+export const register = async (data: { username: string; account: string; password: string }) =>
+    await axios.post(`${REGISTER_URL}`, data);
